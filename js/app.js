39fc0adf784199db21cdbd986cc7d6717733e7a1
Miguel Cobian <miguelcobian99@gmail.com>
2024-01-23 21:01:25 -0600

    Validación de formulario

---
+++ js/app.js
@@ -0,0 +1,38 @@
+let cliente = {
+    mesa: '',
+    hora: '',
+    pedido: [],
+};
+
+const btnGuardarCliente = document.querySelector('#guardar-cliente');
+btnGuardarCliente.addEventListener('click', guardarCliente);
+
+function guardarCliente() {
+    const mesa = document.querySelector('#mesa').value;
+    const hora = document.querySelector('#hora').value;
+
+    // Revisar si hay campos vacios
+    const camposVacios = [mesa, hora].some(campo => campo === '');
+
+    if (camposVacios) {
+        // Verificar si ya hay una alerta
+        const existeAlerta = document.querySelector('.invalid-feedback');
+        if (!existeAlerta) {
+            const alerta = document.createElement('DIV');
+            alerta.classList.add('invalid-feedback', 'd-block', 'text-center');
+            alerta.textContent = 'Todos los campos son obligatorios';
+            document.querySelector('.modal-body form').appendChild(alerta);
+
+            setTimeout(() => {
+                alerta.remove()
+            }, 3000);
+
+        }
+
+        return;
+    }
+
+
+
+};
+
